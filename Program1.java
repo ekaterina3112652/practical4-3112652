6fd8c4dbab66a1674fb56b2f29d84506907c964c
Ekaterina Str <ekaterina.strazdina@student.griffith.ie>
2023-04-19 16:37:48 +0100

    add the third int and output all three of them

--- Program1.java
+++ Program1.java
@@ -4,11 +4,12 @@ public class Program1 {
     public static void main(String[] args) {
         //take in two integers from the command line
         Scanner scan = new Scanner(System.in);
-        System.out.print("\nEnter two int: ");
+        System.out.print("\nEnter three int: ");
         int int1 = scan.nextInt();
         int int2 = scan.nextInt();
+        int int3 = scan.nextInt();
         //print out the values entered
-        System.out.println("You entered: " + int1 + " and " + int2);
+        System.out.println("You entered: " + int1 + ", " + int2 + " and " + int3);
         scan.close();
     }
 }
