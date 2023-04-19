4927744d985080dfd4d0840417358cdd3f267a2d
Ekaterina Str <ekaterina.strazdina@student.griffith.ie>
2023-04-19 15:56:24 +0100

    take in two integers from user and output them

---
+++ Program1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Program1 {
+    public static void main(String[] args) {
+        //take in two integers from the command line
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\nEnter two int: ");
+        int int1 = scan.nextInt();
+        int int2 = scan.nextInt();
+        //print out the values entered
+        System.out.println("You entered: " + int1 + " and " + int2);
+        scan.close();
+    }
+}
